5fe2afcc7dfe5ef0a67d39a9d07a9b42051b0c77
lihongjie <lihongjie@axera-tech.com>
2026-06-08 17:34:58 +0800

    llm_build换成llm_build2

--- model_convert/build_llm_0.8b.sh
+++ model_convert/build_llm_0.8b.sh
@@ -3,20 +3,13 @@ export FLOAT_MATMUL_USE_CONV_EU=1
 
 INPUT_DIR=../../Qwen/Qwen3.5-0.8B-GPTQ-Int4-EN
 OUTPUT_DIR=../../Qwen3_5.AXERA/ax-llm/Qwen3.5-0.8B-GPTQ-Int4-AX650-C128-P1280-CTX2047-EN
-pulsar2 llm_build --input_path $INPUT_DIR \
+
+pulsar2 llm_build2 --input_path $INPUT_DIR \
                 --output_path  $OUTPUT_DIR \
-                --kv_cache_len 2047 \
                 --hidden_state_type bf16 \
-                --prefill_len 128 \
-                --last_kv_cache_len 128 \
-                --last_kv_cache_len 256 \
-                --last_kv_cache_len 384 \
-                --last_kv_cache_len 512 \
-                --last_kv_cache_len 640 \
-                --last_kv_cache_len 768 \
-                --last_kv_cache_len 896 \
-                --last_kv_cache_len 1024 \
-                --last_kv_cache_len 1152 \
+                --prefill_len 1280 \
+                --prefill_step_size 128 \
+                --max_context 2048 \
                 --chip AX650 \
                 --parallel 8 
 
--- model_convert/build_llm_longctx.sh
+++ model_convert/build_llm_longctx.sh
@@ -5,38 +5,13 @@ export FLOAT_MATMUL_USE_CONV_EU=1
 
 INPUT_DIR=../../Qwen/Qwen3.5-0.8B
 OUTPUT_DIR=../../Qwen3_5.AXERA/ax-llm/Qwen3.5-0.8B-AX650-C256-P6K-CTX8K
-pulsar2 llm_build --input_path $INPUT_DIR \
+
+pulsar2 llm_build2 --input_path $INPUT_DIR \
                 --output_path  $OUTPUT_DIR \
                 --hidden_state_type bf16 \
-                --prefill_len 256 \
-                --last_kv_cache_len 256 \
-                --last_kv_cache_len 512 \
-                --last_kv_cache_len 768 \
-                --last_kv_cache_len 1024 \
-                --last_kv_cache_len 1280 \
-                --last_kv_cache_len 1536 \
-                --last_kv_cache_len 1792 \
-                --last_kv_cache_len 2048 \
-                --last_kv_cache_len 2304 \
-                --last_kv_cache_len 2560 \
-                --last_kv_cache_len 2816 \
-                --last_kv_cache_len 3072 \
-                --last_kv_cache_len 3328 \
-                --last_kv_cache_len 3584 \
-                --last_kv_cache_len 3840 \
-                --last_kv_cache_len 4096 \
-                --last_kv_cache_len 4352 \
-                --last_kv_cache_len 4608 \
-                --last_kv_cache_len 4864 \
-                --last_kv_cache_len 5120 \
-                --last_kv_cache_len 5376 \
-                --last_kv_cache_len 5632 \
-                --last_kv_cache_len 5888 \
-                --last_kv_cache_len 6144 \
-                --kv_cache_len 2047 \
-                --kv_cache_len 4095 \
-                --kv_cache_len 6143 \
-                --kv_cache_len 8191 \
+                --prefill_len 6400 \
+                --prefill_step_size 256 \
+                --max_context 8192 \
                 --chip AX650 \
                 --parallel 8 
 
